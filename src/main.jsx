d5e42575193710b6a5802ed4a97a6110861d5fe2
unknown <v.rapidtech.rizat@adira.co.id>
2023-10-08 15:54:23 +0700

    fixing import scroll to top

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./states/index.js";
 import "react-loading-skeleton/dist/skeleton.css";
-import ScrollToTop from "./utils/scrollToTop.jsx";
+import ScrollToTop from "./utils/ScrollToTop.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
